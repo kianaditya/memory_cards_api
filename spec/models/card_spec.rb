804db962e64680ec0ddf678305e011152e86ea5d
Aditya Naik <naik.aditya@gmail.com>
2019-02-14 14:06:01 +0100

    Adds spec test, test going green

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe Card, type: :model do
   describe 'Database table' do
     it { is_expected.to have_db_column :question }
     it { is_expected.to have_db_column :answer }
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
   end
+
   describe 'Validations' do
     it { is_expected.to validate_presence_of :question }
     it { is_expected.to validate_presence_of :answer }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:card)).to be_valid
+    end
   end
 end
