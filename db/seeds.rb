66a41350368492b09c3290f519bd39b49abb8ba1
Aditya Naik <naik.aditya@gmail.com>
2019-02-14 14:54:07 +0100

    Adds category model, updates tests, updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-Card.destroy_all
-
 require 'faker'
+
+Card.destroy_all
+Category.destroy_all
+User.destroy_all
+
+
+5.times do
+    Category.create(name: Faker::Lorem.word)
+end
 
 10.times do 
     Card.create(question: Faker::Quote.famous_last_words ,answer: Faker::Quote.yoda )
 end
+
+user = User.create(email: "user@mail.com",password: "password")
