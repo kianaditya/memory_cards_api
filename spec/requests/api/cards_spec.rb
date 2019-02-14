43fbeb01c807255778741eba9f194173828260a1
Aditya Naik <naik.aditya@gmail.com>
2019-02-14 13:26:37 +0100

    Updates controller, tests and controller test going green

--- spec/requests/api/cards_spec.rb
+++ spec/requests/api/cards_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Api::V0::PingsController, type: :request do
+RSpec.describe Api::CardsController, type: :request do
   describe 'GET /cards' do
+
+    before do
+      5.times { create(:card) }
+    end
+
     it 'should return all cards' do
       get '/api/cards'
-
       json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
-      expect(json_response['message']).to eq 'Pong'
+      expect(json_response['cards'].count).to eq 5
     end
   end
 end
