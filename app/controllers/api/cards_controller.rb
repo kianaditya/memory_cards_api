1c84df9b1e6a33b19c3b282cbec92f3e7ec76d79
Aditya Naik <naik.aditya@gmail.com>
2019-02-14 17:17:16 +0100

    Adds serializer to structure json response, updates cards_spec test, all tests going green

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -2,6 +2,6 @@ class Api::CardsController < ApplicationController
 
     def index
         cards = Card.all
-        render json: {cards: cards}
+        render json: cards, each_serializer: Cards::IndexSerializer
     end
 end
--- spec/requests/api/cards_spec.rb
+++ spec/requests/api/cards_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::CardsController, type: :request do
-  describe 'GET /cards' do
+  context 'GET /cards' do
 
     let!(:card) {5.times { create(:card) }}
 
@@ -14,7 +14,32 @@ RSpec.describe Api::CardsController, type: :request do
     end
 
     it 'should return all cards' do
-      expect(JSON.parse(response.body)['cards'].count).to eq 5
+      expect(JSON.parse(response.body).count).to eq 5
+    end
+  end
+
+  context 'detailed spec to test associations etc' do
+    let(:user) { create(:user, email: 'user@mail.com') }
+    let(:category) { create(:category, name: 'Ruby') }
+    let!(:card) { create(:card, question:"What's in name?",answer: "n,a,m,e",category: category, user: user) }
+
+    before do
+      get '/api/cards'
+    end
+
+    it 'includes info about user' do
+      expected_email = JSON.parse(response.body)[0]['user']['email']
+      expect(expected_email).to eq 'user@mail.com' 
+    end
+
+    it 'includes info about question' do
+      expected_question = JSON.parse(response.body)[0]['question']
+      expect(expected_question).to eq "What's in name?" 
+    end
+
+    it 'includes info about category' do
+      expected_category = JSON.parse(response.body)[0]['category']['name']
+      expect(expected_category).to eq 'Ruby' 
     end
   end
 end
